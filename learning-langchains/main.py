4c51958527a8cb02c834a188b0ae9411af4fcd0b
Yan <100032599+whykay-01@users.noreply.github.com>
2023-06-28 12:08:29 +0800

    wip: learning how to work with the package

--- learning-langchains/main.py
+++ learning-langchains/main.py
@@ -1,4 +1,4 @@
-from langchain import Cohere
+from langchain.llms import Cohere
 import dotenv
 import os
 
@@ -6,21 +6,6 @@ dotenv.load_dotenv()
 
 COHERE_API_KEY = os.getenv("COHERE_API_KEY")
 
-# cohere = Cohere(model="gptd-instruct-tft", cohere_api_key=COHERE_API_KEY)
-
-# response = cohere.generate("Hello, my name is Yan")
-# print(response)
-
-import cohere
-
-co = cohere.Client(COHERE_API_KEY)
-response = co.generate(
-    model="command",
-    prompt=input("Please enter a prompt: ") + "\n",
-    max_tokens=100,
-    temperature=0.9,
-    k=0,
-    stop_sequences=[],
-    return_likelihoods="NONE",
-)
-print("Response: {}".format(response.generations[0].text))
+cohere = Cohere(model="command", cohere_api_key=COHERE_API_KEY)
+response = cohere("Tell me a joke.")
+print(response)
