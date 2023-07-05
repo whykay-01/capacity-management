eae19af4b1135a98aff8b931b760d25ced68b140
Yan <100032599+whykay-01@users.noreply.github.com>
2023-07-05 10:49:54 +0800

    fix: added the function description comments

--- loaning-system/app/non_unique_user_usage.py
+++ loaning-system/app/non_unique_user_usage.py
@@ -5,7 +5,13 @@ import plotly.graph_objects as go
 
 
 def generate_non_unique_user_df(non_unique_user_equipment_df, user_cycle_df):
-    
+    """
+    This function is used to generate the non-unique user dataframe.
+    :param non_unique_user_equipment_df: dataframe of non-unique user equipment
+    :param user_cycle_df: dataframe of user cycle
+    :return: dataframe of non-unique user equipment
+    """
+
     # turning the dictionary back into a dataframe and then sorting it (for non-unique use equipment)
     non_unique_equipment_usertype_df = fill_dict_user_equipment(non_unique_user_equipment_df, user_cycle_df, "Users")
     
@@ -22,8 +28,12 @@ def generate_non_unique_user_df(non_unique_user_equipment_df, user_cycle_df):
 
 
 def generate_non_unique_user_equipment_bar(non_unique_user_equipment_df, user_cycle_df):
-    # non-unique equipment bar graph
-
+    """
+    This function generates the non-unique user equipment bar graph.
+    :param non_unique_user_equipment_df: dataframe of non-unique user equipment
+    :param user_cycle_df: dataframe of user cycle
+    :return: bar graph of non-unique user equipment
+    """
     non_unique_final_df = generate_non_unique_user_df(non_unique_user_equipment_df, user_cycle_df)
     non_unique_equipment_bar_total = non_unique_final_df.groupby("Equipment").sum(numeric_only=True)
     fig_non_unique_equipment_bar = px.histogram(
