c92366671a1ffdf9366fd86ec9c3b5a1b4a63468
Yan <100032599+whykay-01@users.noreply.github.com>
2023-07-05 11:56:12 +0800

    wip: reduced redundant code

--- loaning-system/dashboard.py
+++ loaning-system/dashboard.py
@@ -5,16 +5,10 @@ import plotly.graph_objects as go
 # Using flask to host the app
 from flask import (
     Flask,
-    flash,
     render_template,
-    request,
-    url_for,
-    redirect,
-    Response,
 )
 
 from app.utils import (load_dataframes, 
-                       update_graph_layouts,
                        fill_dict_user_equipment)
 from app.top5_bar_charts import (generate_top_5_bar_chart)
 from app.pie_chart import (generate_fig_pie)
@@ -55,12 +49,6 @@ def dashboard():
     # monthly check out graph: GRAPHS
     fig_time_monthly = generate_fig_time_cycle_month(equipment_cycle_df)
 
-    # graph layouts and their background features
-    colors = {"background": "#ADD8E6", "text": "#111111"}
-
-    # setting the layout for the graphs
-    # update_graph_layouts(fig_pie, fig_top_5_bar, fig_least_5_bar, fig_non_unique_equipment_bar, fig_time_daily, fig_time_monthly, colors)
-
     print("Dashboard has been generated")
 
     return {"Non-Unique User Pie Chart": fig_pie,
@@ -69,70 +57,6 @@ def dashboard():
             "Used Equipment by non-unique Users": fig_non_unique_equipment_bar,
             "Daily Equipment Timeline": fig_time_daily,
             "Monthly Equipment Timeline": fig_time_monthly}
-    # # webpage layout
-    # app.layout = html.Div(
-    #     style={"backgroundColor": colors["background"]},
-    #     children=[
-    #         html.H1(
-    #             children="Loaning Dashboard",
-    #             style={"textAlign": "center", "color": colors["text"]},
-    #         ),
-    #         html.H4(
-    #             children="Add new CSV files to the system: ",
-    #             style={"textAlign": "left", "color": colors["text"]},
-    #         ),
-    #         dcc.Upload(
-    #             id="upload-data",
-    #             children=html.Div(["Drag and Drop or ", html.A("Select Files")]),
-    #             style={
-    #                 "width": "50%",
-    #                 "height": "60px",
-    #                 "lineHeight": "60px",
-    #                 "borderWidth": "2px",
-    #                 "borderStyle": "dashed",
-    #                 "borderRadius": "5px",
-    #                 "textAlign": "left",
-    #                 "margin": "10px",
-    #             },
-    #             multiple=True,
-    #         ),
-    #         html.H3(
-    #             children="Non-Unique User Pie Chart",
-    #             style={"textAlign": "center", "color": colors["text"]},
-    #         ),
-    #         dcc.Graph(id="pie-graph", figure=fig_pie),
-    #         html.H3(
-    #             children="5 Most Used Equipment (Unique)",
-    #             style={"textAlign": "center", "color": colors["text"]},
-    #         ),
-    #         dcc.Graph(id="top-5-graph", figure=fig_top_5_bar),
-    #         html.H3(
-    #             children="5 Least Used Equipment (Unique)",
-    #             style={"textAlign": "center", "color": colors["text"]},
-    #         ),
-    #         dcc.Graph(id="least-5-graph", figure=fig_least_5_bar),
-    #         html.H3(
-    #             children="Used Equipment by non-unique Users",
-    #             style={"textAlign": "center", "color": colors["text"]},
-    #         ),
-    #         dcc.Graph(
-    #             id="non_unique_equipment_bar", figure=fig_non_unique_equipment_bar
-    #         ),
-    #         html.H3(
-    #             children="Daily Equipment Timeline",
-    #             style={"textAlign": "center", "color": colors["text"]},
-    #         ),
-    #         dcc.Graph(id="-cycle-timeline-graph", figure=fig_time_daily),
-    #         html.H3(
-    #             children="Monthly Equipment Timeline",
-    #             style={"textAlign": "center", "color": colors["text"]},
-    #         ),
-    #         dcc.Graph(id="monthly-timeline-graph", figure=fig_time_monthly),
-    #     ],
-    # )
-
-    # app.run_server(host="0.0.0.0", port=8050, debug=True)
-
 
 @app.route("/")
 def index():
