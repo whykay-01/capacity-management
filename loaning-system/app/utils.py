0b295bd833cdaecb5a439115b04d78920107cfa6
Yan <100032599+whykay-01@users.noreply.github.com>
2023-07-05 10:46:01 +0800

    wip: moved function back to utils

--- loaning-system/app/utils.py
+++ loaning-system/app/utils.py
@@ -30,6 +30,55 @@ def load_dataframes(volume_mountpoint = '/data'):
     return [user_cycle_df, unique_user_equipment_df, non_unique_user_equipment_df, equipment_cycle_df]
 
 
+
+def fill_dict_user_equipment(dataframe, user_cycle_df, user_type):
+    """
+    this function fills the dictionary with the equipment and the number of unique users that have used it
+    :param df: dataframe of the user_equipment database
+    :param user_cycle_df: dataframe of the user_cycle database
+    :param user_type: string of the user type {'Unique Users', 'Users'}
+    :return: dictionary of equipment and number of users
+    """
+
+    equipment_usertype_dict = {"Equipment": [], "User Type": [], "Count": []}
+
+    for i in range(len(dataframe["Equipment"])):
+        user_list = dataframe[user_type][i].split(", ")
+
+        for j in user_list:
+            if (user_cycle_df.loc[user_cycle_df["Unique Users"] == j, "User Type"].iloc[0]== "Staff"):
+
+                equipment_usertype_dict["Equipment"].append(dataframe["Equipment"][i])
+                equipment_usertype_dict["User Type"].append("Staff")
+                equipment_usertype_dict["Count"].append(1)
+
+            elif (user_cycle_df.loc[user_cycle_df["Unique Users"] == j, "User Type"].iloc[0]== "Student"):
+
+                equipment_usertype_dict["Equipment"].append(dataframe["Equipment"][i])
+                equipment_usertype_dict["User Type"].append("Student")
+                equipment_usertype_dict["Count"].append(1)
+
+            elif (user_cycle_df.loc[user_cycle_df["Unique Users"] == j, "User Type"].iloc[0] == "Faculty"):
+
+                equipment_usertype_dict["Equipment"].append(dataframe["Equipment"][i])
+                equipment_usertype_dict["User Type"].append("Faculty")
+                equipment_usertype_dict["Count"].append(1)
+
+            elif (user_cycle_df.loc[user_cycle_df["Unique Users"] == j, "User Type"].iloc[0] == "IT"):
+
+                equipment_usertype_dict["Equipment"].append(dataframe["Equipment"][i])
+                equipment_usertype_dict["User Type"].append("IT")
+                equipment_usertype_dict["Count"].append(1)
+
+            else:
+                
+                equipment_usertype_dict["Equipment"].append(dataframe["Equipment"][i])
+                equipment_usertype_dict["User Type"].append("Other")
+                equipment_usertype_dict["Count"].append(1)
+
+    return pd.DataFrame(data=equipment_usertype_dict)
+
+
 def update_graph_layouts(fig_pie, 
                          fig_top_5_bar, 
                          fig_least_5_bar, 
